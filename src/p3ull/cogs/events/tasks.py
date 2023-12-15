599bcf95df769918c12a02cd9eaa1357cda70b96
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2023-12-14 19:29:46 -0500

    Update tasks.py

--- src/p3ull/cogs/events/tasks.py
+++ src/p3ull/cogs/events/tasks.py
@@ -16,6 +16,7 @@ URL = "https://api.dexscreener.com/latest/dex/tokens/0xa77e70d0af1ac7ff86726740d
 @plugin.register_loop(wait_until_ready=True)
 @tasks.loop(minutes=2)
 async def price_update():
+    logging.info("updating price")
     async with aiohttp.request("GET", URL, headers=headers) as r:
         data = await r.json()
         price = data["pairs"][0]["priceUsd"]
