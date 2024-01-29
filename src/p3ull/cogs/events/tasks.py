d1172640384a41fac9ff9654a53a4ba44bd124d0
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-01-29 08:11:01 -0500

    Update tasks.py

--- src/p3ull/cogs/events/tasks.py
+++ src/p3ull/cogs/events/tasks.py
@@ -1,3 +1,4 @@
+import logging
 import aiohttp
 from disnake.ext import plugins, tasks
 
@@ -16,7 +17,6 @@ URL = "https://api.dexscreener.com/latest/dex/tokens/0xa77e70d0af1ac7ff86726740d
 @plugin.register_loop(wait_until_ready=True)
 @tasks.loop(minutes=2)
 async def price_update():
-    logging.info("updating price")
     async with aiohttp.request("GET", URL, headers=headers) as r:
         data = await r.json()
         price = data["pairs"][0]["priceUsd"]
